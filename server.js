5cbb64ea74c6e3be56a0cb71190581922290e377
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-05-20 16:16:10 +0300

    added database connection string for heroku

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'admin',
-      database : 'facerecognition'
+      connectionString : process.env.DATABASE_URL,
+      ssl: true 
     }
   });
 
