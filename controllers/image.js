2a707175e12476b481ab0770ffcfe2855780fcab
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-05-16 16:02:27 +0300

    chore: update clarifai model versions with request calls to use images URL

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,51 @@
-const  Clarifai = require('clarifai');
+// const  Clarifai = require('clarifai');
 
 // clarifai api key
-const app = new Clarifai.App({
-    apiKey: 'ba2d8090585c4e208d7e59199456ddaf'
-   });
+// const app = new Clarifai.App({
+//     apiKey: 'ba2d8090585c4e208d7e59199456ddaf'
+//    });
 
 const handleApiCall = (req, res) => {
-    app.models.predict('face-detection', req.body.input)
-    .then(data => {
-        res.json(data)
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
+
+    const PAT = '250c71f2f384418c9edd310fb8339caf';
+    const USER_ID = 'benkyam';
+    const APP_ID = 'facerecognition';
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = req.body.id;
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    // app.models.predict('face-detection', req.body.input)
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
 }
    
 
